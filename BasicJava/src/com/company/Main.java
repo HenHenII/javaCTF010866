7fae55ad25bf46401c3e7b30375b6cd7a421593c
HenHenII <77061659+HenHenII@users.noreply.github.com>
2021-04-09 12:05:12 +0700

    java example 2.1

---
+++ BasicJava/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//java 2.1
+        //java 2.1.1
+          //Hello world
+         /*
+           hello
+           myname's mark
+           I'm 21 years old
+          */
+        //java 2.1.2
+          int mark1 =10;
+          boolean mark2 = true;
+          String mark3 = "mark";
+          double mark4 = 1.1d;
+          float mark5 = 2.2f;
+        //java 2.1.3
+          System.out.println("This is a int "+ mark1);
+          System.out.println("This is a boolean "+ mark2);
+          System.out.println("This is a String "+ mark3);
+          System.out.println("This is a double "+ mark4);
+          System.out.println("This is a float "+ mark5);
+    }
+}
